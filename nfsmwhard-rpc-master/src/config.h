fa3cccd9590591ae79acb90b6be6c2b81be55c46
Alperen Aydın <108737692+rybetasz@users.noreply.github.com>
2024-01-30 09:56:37 +0300

    Update config.h

--- nfsmwhard-rpc-master/src/config.h
+++ nfsmwhard-rpc-master/src/config.h
@@ -1,6 +1,6 @@
 #define APP_ID "1077644312701042780"
 #define IMG_KEY "ads556"
-#define IMG_TXT "Need for Speed Most Wanted Hard Plus"
+#define IMG_TXT "Need for Speed: Most Wanted - Hard Plus"
 #define UPD_INTVL 5000
 
 #define IN_MENU "Main Menu" // ANY menu
